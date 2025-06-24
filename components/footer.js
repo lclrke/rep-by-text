568bad19d8bbf818e73cc27abed28cc02d082101
lclrke <liamclarke@outlook.com>
2025-06-24 16:43:41 -0400

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -1,4 +1,3 @@
-import Dropzone from "components/dropzone";
 import {
   Code as CodeIcon,
   Download as DownloadIcon,
@@ -7,12 +6,11 @@ import {
 } from "lucide-react";
 import Link from "next/link";
 
-export default function Footer({ events, startOver, handleImageDropped }) {
+export default function Footer({ events, startOver }) {
   return (
     <footer className="w-full my-8">
       <div className="text-center">
         <Link href="/about" className="lil-button">
-
           <InfoIcon className="icon" />What is this?
         </Link>
 
@@ -23,49 +21,51 @@ export default function Footer({ events, startOver, handleImageDropped }) {
           </button>
         )}
 
-        <Dropzone onImageDropped={handleImageDropped} />
-
-        {events.length > 2 && (
-          (<Link
-            href={events.findLast((ev) => ev.image).image}
+        {/* Show download button if we have generated audio */}
+        {events.length > 1 && events.some(ev => ev.audio) && (
+          <Link
+            href={events.findLast((ev) => ev.audio).audio}
             className="lil-button"
             target="_blank"
-            rel="noopener noreferrer">
-
-            <DownloadIcon className="icon" />Download image
-          </Link>)
+            rel="noopener noreferrer"
+            download
+          >
+            <DownloadIcon className="icon" />Download music
+          </Link>
         )}
 
         <Link
-          href="https://github.com/replicate/paint-by-text"
+          href="https://github.com/lclrke/rep-by-text"
           className="lil-button"
           target="_blank"
-          rel="noopener noreferrer">
-
+          rel="noopener noreferrer"
+        >
           <CodeIcon className="icon" />Fork repo
         </Link>
       </div>
 
       <div className="text-center lil-text mt-8">
-      <div className="inline-block py-2 px-4 border border-yellow-200 rounded-lg bg-[#fef6aa]">
-      🤔 Are you a developer and want to learn how to build this? Check out the{" "}
-        <Link
-          href="https://github.com/replicate/paint-with-words#readme"
-          target="_blank">
-          README
-        </Link>.
-      </div>
+        <div className="inline-block py-2 px-4 border border-yellow-200 rounded-lg bg-[#fef6aa]">
+          🎵 Want to learn how to build AI music generators? Check out the{" "}
+          <Link
+            href="https://github.com/lclrke/rep-by-text#readme"
+            target="_blank"
+          >
+            README
+          </Link>.
+        </div>
       </div>
 
       <div className="text-center lil-text mt-8">
         Powered by{" "}
-        <Link href="https://replicate.com/black-forest-labs" target="_blank">
-          Black Forest Labs
+        <Link href="https://replicate.com/lclrke" target="_blank">
+          Your Fine-tuned Model
         </Link>
         ,{" "}
         <Link
-          href="https://replicate.com/blog/flux-kontext?utm_source=project&utm_campaign=paintbytext"
-          target="_blank">
+          href="https://replicate.com?utm_source=project&utm_campaign=musicbytext"
+          target="_blank"
+        >
           Replicate
         </Link>
         ,{" "}
@@ -73,7 +73,7 @@ export default function Footer({ events, startOver, handleImageDropped }) {
           Vercel
         </Link>
         , and{" "}
-        <Link href="https://github.com/replicate/paint-by-text" target="_blank">
+        <Link href="https://github.com/lclrke/rep-by-text" target="_blank">
           GitHub
         </Link>
       </div>
